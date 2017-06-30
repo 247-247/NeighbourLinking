5d07678cc806fc35bf23f1629b75dcac07735dea
Waqar Ahmed <waqar.ahmed5571@gmail.com>
2017-06-30 21:19:59 +0500

    Remove error araise me on offer module

--- NeighbourLinking/app/src/main/java/com/example/waqarahmed/neighbourlinking/Activities/Offers.java
+++ NeighbourLinking/app/src/main/java/com/example/waqarahmed/neighbourlinking/Activities/Offers.java
@@ -3,6 +3,7 @@ package com.example.waqarahmed.neighbourlinking.Activities;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.view.MenuItem;
 
 //import com.example.waqarahmed.neighbourlinking.Classes.Pager;
 import com.example.waqarahmed.neighbourlinking.Classes.Pager_offers;
+import com.example.waqarahmed.neighbourlinking.Fragments.Home_Wall;
 import com.example.waqarahmed.neighbourlinking.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -18,8 +20,8 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class Offers extends AppCompatActivity {
 
-    static ViewPager viewPager_offer;
-    TabLayout tabLayout_offer;
+ /*   static ViewPager viewPager_offer;
+    TabLayout tabLayout_offer;*/
     DatabaseReference mDatabaseReferenceUser , mDatabaseCurrentUser;
     FirebaseAuth mAuth;
     FirebaseAuth.AuthStateListener authStateListener;
@@ -32,6 +34,9 @@ public class Offers extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setTitle("Current Offer");
+
+        // Never Remove it bcx may i need it in future
+
 //        tabLayout_offer= (TabLayout) findViewById(R.id.tabLayout_offer);
 //        viewPager_offer= (ViewPager) findViewById(R.id.pager_offer);
 //        tabLayout_offer.addTab(tabLayout_offer.newTab().setText("Home"));
@@ -44,6 +49,10 @@ public class Offers extends AppCompatActivity {
 //        tabLayout_offer.setOnTabSelectedListener(new Offers.TabListners());
         mAuth = FirebaseAuth.getInstance();
         mDatabaseReferenceUser = FirebaseDatabase.getInstance().getReference().child("User");
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        Home_Wall home_wall = new Home_Wall();
+        fragmentTransaction.replace(R.id.fragment_container,home_wall);
+        fragmentTransaction.commit();
 
 
 
@@ -52,24 +61,24 @@ public class Offers extends AppCompatActivity {
     }
 
 
-    public static class TabListners implements TabLayout.OnTabSelectedListener{
-
-        @Override
-        public void onTabSelected(TabLayout.Tab tab) {
-            viewPager_offer.setCurrentItem(tab.getPosition());
-        }
-
-        @Override
-        public void onTabUnselected(TabLayout.Tab tab) {
-
-        }
-
-        @Override
-        public void onTabReselected(TabLayout.Tab tab) {
-
-        }
-    }
-
+//    public static class TabListners implements TabLayout.OnTabSelectedListener{
+//
+//        @Override
+//        public void onTabSelected(TabLayout.Tab tab) {
+//            viewPager_offer.setCurrentItem(tab.getPosition());
+//        }
+//
+//        @Override
+//        public void onTabUnselected(TabLayout.Tab tab) {
+//
+//        }
+//
+//        @Override
+//        public void onTabReselected(TabLayout.Tab tab) {
+//
+//        }
+//    }
+//
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
