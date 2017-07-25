5d5439df65fbb09244d9348e77d9089037f128dc
Waqar Ahmed <waqar.ahmed5571@gmail.com>
2017-07-26 02:24:27 +0500

    nav header change

--- NeighbourLinking/app/src/main/java/com/example/waqarahmed/neighbourlinking/Services/PlaceRequestOnServer.java
+++ NeighbourLinking/app/src/main/java/com/example/waqarahmed/neighbourlinking/Services/PlaceRequestOnServer.java
@@ -8,7 +8,15 @@ import android.util.Log;
 import android.widget.Toast;
 
 import com.example.waqarahmed.neighbourlinking.Activities.TanantActivities.MainActivity;
+import com.example.waqarahmed.neighbourlinking.Classes.AppStatus;
+import com.example.waqarahmed.neighbourlinking.Classes.AppUtils;
+import com.example.waqarahmed.neighbourlinking.Classes.Brand;
 import com.example.waqarahmed.neighbourlinking.R;
+import com.example.waqarahmed.neighbourlinking.Shared.BrandSharedPref;
+import com.google.common.base.Strings;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -28,7 +36,7 @@ import java.net.URLEncoder;
  * Created by Waqar ahmed on 6/1/2017.
  */
 
-public class PlaceRequestOnServer extends AsyncTask<String, Void,  Void> {
+public class PlaceRequestOnServer extends AsyncTask<String, Void,  String> {
 
     Context cxt;
     ProgressDialog progress ;
@@ -39,16 +47,28 @@ public class PlaceRequestOnServer extends AsyncTask<String, Void,  Void> {
     @Override
     protected void onPreExecute() {
         super.onPreExecute();
-        progress = new ProgressDialog(cxt);
-        progress.setTitle("Loading");
-        progress.setMessage("Wait while loading...");
-        progress.setCancelable(false); // disable dismiss by tapping outside of the dialog
-        progress.show();
+
+        if (AppStatus.getInstance(cxt).isOnline()) {
+            if (progress == null) {
+                progress = new ProgressDialog(cxt);
+                progress.setTitle("Loading");
+                progress.setMessage("Wait while loading...");
+                progress.setCancelable(false); // disable dismiss by tapping outside of the dialog
+                progress.show();
+            } else {
+              //  progress.show();
+            }
+
+        }
+//        progress.setTitle("Loading");
+//        progress.setMessage("Wait while loading...");
+//        progress.setCancelable(false); // disable dismiss by tapping outside of the dialog
+//        progress.show();
 
     }
 
     @Override
-    protected Void doInBackground(String... strings) {
+    protected String doInBackground(String... strings) {
         String type, cause, sender_id, owner_house_address, owner_contact, owner_name, owner_image_url, powerMan_name, powerMan_image_url, poweMan_id;
 
         type = strings[0];
@@ -61,76 +81,120 @@ public class PlaceRequestOnServer extends AsyncTask<String, Void,  Void> {
         powerMan_name = strings[7];
         powerMan_image_url = strings[8];
         poweMan_id = strings[9];
-        String baseUrl = cxt.getResources().getString(R.string.baseUrl);
-        String url_string = baseUrl+"/Neighbour/public/newRequestInsert";
-        try {
-            Log.i("TAG", "doInBackground:  "+type+" "+cause+" "+sender_id+" "+owner_house_address);
-            URL url = null;
+        if (AppStatus.getInstance(cxt).isOnline()) {
+            String baseUrl = cxt.getResources().getString(R.string.baseUrl);
+            String url_string = baseUrl + "/Neighbour/public/newRequestInsert";
             try {
-                url = new URL(url_string);
-            } catch (MalformedURLException e) {
+                Log.i("TAG", "doInBackground:  " + type + " " + cause + " " + sender_id + " " + owner_house_address);
+                URL url = null;
+                try {
+                    url = new URL(url_string);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setRequestMethod("POST");
+
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode(type, "UTF-8") + "&" +
+                        URLEncoder.encode("cause", "UTF-8") + "=" + URLEncoder.encode(cause, "UTF-8") + "&" +
+                        URLEncoder.encode("sender_id", "UTF-8") + "=" + URLEncoder.encode(sender_id, "UTF-8") + "&" +
+                        URLEncoder.encode("owner_house_address", "UTF-8") + "=" + URLEncoder.encode(owner_house_address, "UTF-8") + "&" +
+                        URLEncoder.encode("owner_contact", "UTF-8") + "=" + URLEncoder.encode(owner_contact, "UTF-8") + "&" +
+                        URLEncoder.encode("owner_name", "UTF-8") + "=" + URLEncoder.encode(owner_name, "UTF-8") + "&" +
+                        URLEncoder.encode("owner_image_url", "UTF-8") + "=" + URLEncoder.encode(owner_image_url, "UTF-8") + "&" +
+                        URLEncoder.encode("powerMan_name", "UTF-8") + "=" + URLEncoder.encode(powerMan_name, "UTF-8") + "&" +
+                        URLEncoder.encode("powerMan_image_url", "UTF-8") + "=" + URLEncoder.encode(powerMan_image_url, "UTF-8") + "&" +
+                        URLEncoder.encode("powerMan_id", "UTF-8") + "=" + URLEncoder.encode(poweMan_id, "UTF-8");
+                //  bufferedWriter.write(post_data);
+                bufferedWriter.write(post_data);
+                Log.i("TAG", "doInBackground: 7 ");
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = " ";
+                String line = " ";
+                String TAG = "Tag";
+                Log.i(TAG, "doInBackground: 8 ");
+                while ((line = bufferedReader.readLine()) != null) {
+
+                    result += line;
+                    Log.i(TAG, "doInBackground: 9 ");
+
+                }
+                try {
+                    JSONObject jsonRootObject = null;
+                    try {
+
+                        jsonRootObject = new JSONObject(result);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    String jsonObject = jsonRootObject.getString("response");
+
+                    //   int employeeId = jsonObject.getInt("employeeId");
+
+                    if (jsonObject.equals("true")) {
+                        return "yes";
+                    } else {
+
+
+                        return "no";
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
-            Log.i("TAG", "doInBackground: 2 ");
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-            Log.i("TAG", "doInBackground: 3 ");
-            httpURLConnection.setRequestMethod("POST");
-            Log.i("TAG", "doInBackground: 4 ");
-            httpURLConnection.setDoInput(true);
-            httpURLConnection.setDoOutput(true);
-            OutputStream outputStream = httpURLConnection.getOutputStream();
-            Log.i("TAG", "doInBackground: 5 ");
-            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-            String post_data = URLEncoder.encode("type","UTF-8")+"="+URLEncoder.encode(type,"UTF-8")+"&"+
-                                URLEncoder.encode("cause","UTF-8")+"="+URLEncoder.encode(cause, "UTF-8")+"&"+
-                                URLEncoder.encode("sender_id","UTF-8")+"="+URLEncoder.encode(sender_id,"UTF-8")+"&"+
-                                URLEncoder.encode("owner_house_address","UTF-8")+"="+URLEncoder.encode(owner_house_address,"UTF-8")+"&"+
-                                URLEncoder.encode("owner_contact","UTF-8")+"="+URLEncoder.encode(owner_contact,"UTF-8")+"&"+
-                                URLEncoder.encode("owner_name","UTF-8")+"="+URLEncoder.encode(owner_name, "UTF-8")+"&"+
-                                URLEncoder.encode("owner_image_url","UTF-8")+"="+URLEncoder.encode(owner_image_url,"UTF-8")+"&"+
-                                URLEncoder.encode("powerMan_name","UTF-8")+"="+URLEncoder.encode(powerMan_name,"UTF-8")+"&"+
-                                URLEncoder.encode("powerMan_image_url","UTF-8")+"="+URLEncoder.encode(powerMan_image_url,"UTF-8")+"&"+
-                                URLEncoder.encode("powerMan_id", "UTF-8")+"="+URLEncoder.encode(poweMan_id,"UTF-8");
-          //  bufferedWriter.write(post_data);
-            bufferedWriter.write(post_data);
-            Log.i("TAG", "doInBackground: 7 ");
-            bufferedWriter.flush();
-            bufferedWriter.close();
-            outputStream.close();
-            InputStream inputStream = httpURLConnection.getInputStream();
-
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
-            String result=" ";
-            String line =" ";
-            String TAG = "Tag";
-            Log.i(TAG, "doInBackground: 8 ");
-            while( (line = bufferedReader.readLine()) != null){
-
-                result += line;
-                Log.i(TAG, "doInBackground: 9 ");
-
-            }
 
 
 
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+        }else{
+            return "offline";
         }
+
         return null;
     }
 
         @Override
-    protected void onPostExecute(Void aVoid) {
+    protected void onPostExecute(String aVoid) {
         super.onPostExecute(aVoid);
-            progress.dismiss();
-            Toast.makeText(cxt,"Request sended",Toast.LENGTH_SHORT).show();
-            Intent mainIntent = new Intent(cxt,MainActivity.class);
-                      cxt.startActivity(mainIntent);
+            if (progress != null) {
+                progress.dismiss();
+            }
+            if(aVoid.equals("yes")){
+                Toast.makeText(cxt,"Request sended",Toast.LENGTH_SHORT).show();
+                Intent mainIntent = new Intent(cxt,MainActivity.class);
+                cxt.startActivity(mainIntent);
+            }else if(aVoid.equals("no")){
+                Toast.makeText(cxt,"Might be you have already made Request",Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(cxt,"Network error",Toast.LENGTH_SHORT).show();
+            }
+
+
 
 
 
