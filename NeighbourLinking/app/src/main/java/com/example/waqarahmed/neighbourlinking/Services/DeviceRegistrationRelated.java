59883618a30feb1d8c5fd8f5abb49cb0ead4e5d6
waqar247 <waqar.ahmed5571@gmail.com>
2017-11-22 02:04:06 +0500

    device registration error remove

--- NeighbourLinking/app/src/main/java/com/example/waqarahmed/neighbourlinking/Services/DeviceRegistrationRelated.java
+++ NeighbourLinking/app/src/main/java/com/example/waqarahmed/neighbourlinking/Services/DeviceRegistrationRelated.java
@@ -9,7 +9,9 @@ import com.example.waqarahmed.neighbourlinking.R;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import okhttp3.FormBody;
@@ -40,58 +42,38 @@ public class DeviceRegistrationRelated extends AsyncTask<Void,Void,Void>
     }
 
     @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        mAuth = FirebaseAuth.getInstance();
+       // registerToken();
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        currentDateTime= df.format(c.getTime());
+        SharedPreferences appPrefs = context.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        // Log.d("Firebase", "New Token Created" + token);
+        token = appPrefs.getString(Name,"AnyVelue");
+        String s = mAuth.getCurrentUser().getEmail();
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids)
+    {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = new FormBody.Builder().add("device_token",token).add("email",mAuth.getCurrentUser().getEmail()).add("created_at",currentDateTime).add("updated_at",currentDateTime).build();
+        String baseUrl = context.getResources().getString(R.string.baseUrl);
+        Request request = new Request.Builder().url(baseUrl+"/Neighbour/public/DeviceRegistration").post(body).build();
+        try {
+            okhttp3.Response response= client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    @Override
     protected void onPostExecute(Void aVoid) {
         super.onPostExecute(aVoid);
 
 
     }
 
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        mAuth = FirebaseAuth.getInstance();
-//        dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-//        currentDateTime = dateFormat.format(new Date());
-    }
-
-    @Override
-    protected Void doInBackground(Void... voids)
-    {
-
-        SharedPreferences appPrefs = context.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
-       // Log.d("Firebase", "New Token Created" + token);
-        token = appPrefs.getString(Name,"AnyVelue");
-        registerToken(token);
-        Log.i("RRRRRRR Background", "storeTokenOnPrefrences: "+token);
-
-
-//        OkHttpClient client = new OkHttpClient();
-//        RequestBody body = new FormBody.Builder().add("Mes",text).build();
-//        Request request = new Request.Builder().url("http://mywebsit.comli.com/pushMessage.php").post(body).build();
-
-//        try {
-//            client.newCall(request).execute();
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-        return null;
-    }
-    private void registerToken(String token) {
-
-        OkHttpClient client = new OkHttpClient();
-        RequestBody body = new FormBody.Builder().add("device_token",token).add("email",mAuth.getCurrentUser().getEmail()).add("created_at",currentDateTime).add("updated_at",currentDateTime).build();
-        Log.i("RRRRRRRRRRRGGRRRRRRRR", "registerToken: "+token);
-        Log.i("RRRRRRRRRRRGGRRRRRRRR", "registerToken: "+currentDateTime);
-        String baseUrl = context.getResources().getString(R.string.baseUrl);
-        Request request = new Request.Builder().url(baseUrl+"/Neighbour/public/DeviceRegistration").post(body).build();
-
-        try {
-            okhttp3.Response response= client.newCall(request).execute();
-            Log.i("RRRRRRR Response", "registerToken: new CALLL "+request.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.i("LLLLLLLLLLLLLLLLLLLL", "registerToken: new CALLL ");
-        }
-    }
 }
